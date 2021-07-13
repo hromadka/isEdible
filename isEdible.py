b609790d9838c452a6f65f15fc2c4f7a22b8470e
Ted Hromadka <hromadka@gmail.com>
2021-07-13 07:56:01 -0700

    clean up

--- isEdible.py
+++ isEdible.py
@@ -1,14 +1,16 @@
 from googlesearch import search
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 import requests
   
-# to search
+# read csv
+
+
+# search term
 query = "pim-olas"
 
 url = "https://www.google.com/search?q=" + query + "&sourceid=chrome&ie=UTF-8"
 
 response = requests.get(url)
-#print(response.text)
 
 if 'food' in response.text:
     print("found one!")
