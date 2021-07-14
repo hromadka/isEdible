7f5ea41d52dc4f7b0621b62a5613b518b685bf27
Ted Hromadka <hromadka@gmail.com>
2021-07-14 16:01:51 -0700

    food search works for many test dishes, needs more ways to qualify.

--- isEdible.py
+++ isEdible.py
@@ -4,22 +4,27 @@ from googlesearch import search
 import requests
   
 # read csv
-file1 = open('input4.txt', 'r')
+file1 = open('Dish-subset-TEST.csv', 'r')
 Lines = file1.readlines()
 
 # read input; count occurences for conditional probabilties; save test_cases for prediction step
 for line in Lines: #for line in stdin:
+    query = "default"
+    # note that this fails if there is a comma in the field
     x = line.rstrip('\n').split(',')
 
-# search term
-query = "default"
+    # search term
+    query = x[1]  
 
-url = "https://www.google.com/search?q=" + query + "&sourceid=chrome&ie=UTF-8"
+    url = "https://www.google.com/search?q=" + query + "&sourceid=chrome&ie=UTF-8"
 
-response = requests.get(url)
+    response = requests.get(url)
 
-if 'food' in response.text:
-    print("found one!")
+    if 'food' in response.text:
+        print(query, "<-- found one!")
+    else:
+        # TODO add additional search terms 
+        print(query, "<-- DO NOT EAT")
 
 
 # write modified csv
