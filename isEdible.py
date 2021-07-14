c7451119ab37a442c7e52ca72cb97b32868ba536
Ted Hromadka <hromadka@gmail.com>
2021-07-14 15:52:57 -0700

    read csv

--- isEdible.py
+++ isEdible.py
@@ -1,12 +1,18 @@
+
 from googlesearch import search
 #from bs4 import BeautifulSoup
 import requests
   
 # read csv
+file1 = open('input4.txt', 'r')
+Lines = file1.readlines()
 
+# read input; count occurences for conditional probabilties; save test_cases for prediction step
+for line in Lines: #for line in stdin:
+    x = line.rstrip('\n').split(',')
 
 # search term
-query = "pim-olas"
+query = "default"
 
 url = "https://www.google.com/search?q=" + query + "&sourceid=chrome&ie=UTF-8"
 
@@ -16,6 +22,7 @@ if 'food' in response.text:
     print("found one!")
 
 
+# write modified csv
 
 
 
